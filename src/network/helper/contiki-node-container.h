adb372f499edc41c4d400a739a1e14dc3c96246d
Taqi Ad-Din <tkennouc@zimbra.inria.fr>
2013-04-11 16:17:57 +0200

    bugs

---
+++ src/network/helper/contiki-node-container.h
@@ -0,0 +1,21 @@
+/*
+ * contiki-node-container.h
+ *
+ *  Created on: Feb 24, 2013
+ *      Author: kentux
+ */
+
+#ifndef CONTIKI_NODE_CONTAINER_H_
+#define CONTIKI_NODE_CONTAINER_H_
+
+#include "node-container.h"
+
+namespace ns3 {
+
+class ContikiNodeContainer : public NodeContainer {
+public:
+	ContikiNodeContainer();
+};
+}
+
+#endif /* CONTIKI_NODE_CONTAINER_H_ */
